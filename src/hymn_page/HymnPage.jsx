1e2c4a1322bf02e05659c746c42804a2f1f453d5
rwothmio <brwothmio@gmail.com>
2023-09-15 23:16:20 +0300

    remove unused file

--- src/hymn_page/HymnPage.jsx
+++ src/hymn_page/HymnPage.jsx
@@ -2,11 +2,10 @@ import { View } from "react-native";
 import React from "react";
 import Header from './header/Header'
 import Hymn from "./hymn/Hymn";
-import styles from "./styles";
 
 const HymnPage = () => {
   return (
-    <View style={styles.container} >
+    <View>
       <Header />
       <Hymn />
     </View>
